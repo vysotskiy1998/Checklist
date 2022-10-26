421022191b9245e015daecf1fc64a6af40b83234
Vysotskiy Vladimir <user@MBP--Vladimir.N4>
2022-10-26 20:03:42 +0300

    Last Changes

--- Checklist/AddItemViewController.swift
+++ Checklist/AddItemViewController.swift
@@ -7,12 +7,27 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
  
         
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
+    //MARK: Убирает выделение ячейки
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
     }
 
     @IBAction func cancel(_ sender: Any) {
@@ -20,7 +35,29 @@ class AddItemViewController: UITableViewController {
     }
     
     @IBAction func done(_ sender: Any) {
+        
+        print("Content of the text Field \(textField!)")
         navigationController?.popViewController(animated: true)
+    }
+    
+    //MARK: Метод что бы когда поле пустое кнопка Done становилась не активной
+    func textField(
+        _ textField: UITextField,
+        shouldChangeCharactersIn range: NSRange,
+        replacementString string: String
+    ) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        newText.isEmpty ? (doneBarButton.isEnabled = false) : (doneBarButton.isEnabled = true)
+        
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
     }
 }
  

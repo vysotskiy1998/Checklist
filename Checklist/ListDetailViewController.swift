2924275b55b453097d976b16f7a71f97feb93b0a
vysotskiy1998 <vysotskiy.boss@gmail.com>
2022-11-29 09:47:51 +0200

    Added new swift file ListDetailViewController

---
+++ Checklist/ListDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ListDetailViewController.swift
+//  Checklist
+//
+//  Created by Vysotskiy Vladimir on 29.11.2022.
+//
+
+import UIKit
+
+protocol ListViewControllerDelegate: AnyObject {
+    
+}
+
+class ListDetailViewController: UITableViewController, UITextFieldDelegate {
+    
+}

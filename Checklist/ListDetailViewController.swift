e648559480be59b9c8d4aaaf55f66149755accf8
vysotskiy1998 <vysotskiy.boss@gmail.com>
2022-11-29 11:46:57 +0200

    Added new TVC in Storyboard and added buttuns and textfield in class ListDetailViewController

--- Checklist/ListDetailViewController.swift
+++ Checklist/ListDetailViewController.swift
@@ -7,12 +7,20 @@
 
 import UIKit
 
-protocol ListViewControllerDelegate: AnyObject {
+protocol ListDetailViewControllerDelegate: AnyObject {
     func listDetailViewControllerDidCancel(_ controller: ListDetailViewController)
     func listDetailViewController(_ controller: ListDetailViewController, didFinishAdding checklist: Checklist)
     func listDetailViewController(_ controller: ListDetailViewController, didFinishEditing checklist: Checklist)
 }
 
 class ListDetailViewController: UITableViewController, UITextFieldDelegate {
+    
+    @IBOutlet var textField: UITextField!
+    
+    @IBOutlet var doneBarButton: UIBarButtonItem!
+    
+    weak var delegate: ListDetailViewControllerDelegate?
+    
+    var checklistToEdit: Checklist?
     
 }
